156206c8a17198f5098243228c583b273964047b
zhuqiu <zhuqiu@authine.com>
2020-07-10 16:36:18 +0800

    功能新增

--- src/plugins/vant.js
+++ src/plugins/vant.js
@@ -1,6 +1,12 @@
+/*
+ * @Date: 2020-07-10 09:43:26
+ * @LastEditors: zhuqiu
+ * @LastEditTime: 2020-07-10 14:03:52
+ * @FilePath: \project\src\plugins\vant.js
+ */ 
 // 按需全局引入 vant组件
 import Vue from 'vue'
-import { Button, List, Cell, Tabbar, TabbarItem, Swipe, SwipeItem, Grid, GridItem, Search } from 'vant'
+import { Button, List, Cell, Tabbar, TabbarItem, Swipe, SwipeItem, Grid, GridItem, Search, Icon } from 'vant'
 Vue.use(Button)
 Vue.use(Cell)
 Vue.use(List)
@@ -9,4 +15,5 @@ Vue.use(SwipeItem)
 Vue.use(Grid)
 Vue.use(GridItem)
 Vue.use(Search)
+Vue.use(Icon)
 Vue.use(Tabbar).use(TabbarItem)
--- src/views/commonPage/home.vue
+++ src/views/commonPage/home.vue
@@ -1,20 +1,25 @@
 <!--
  * @Date: 2020-07-10 09:43:26
  * @LastEditors: zhuqiu
- * @LastEditTime: 2020-07-10 10:36:24
+ * @LastEditTime: 2020-07-10 14:11:45
  * @FilePath: \project\src\views\commonPage\home.vue
 --> 
 <template>
   <div>
     <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
-      <van-swipe-item>1</van-swipe-item>
-      <van-swipe-item>2</van-swipe-item>
-      <van-swipe-item>3</van-swipe-item>
-      <van-swipe-item>4</van-swipe-item>
+      <van-swipe-item>
+        <img src="../../assets/images/banner01.jpg" style="width:100%" alt="">
+      </van-swipe-item>
+      <van-swipe-item>
+        <img src="../../assets/images/banner01.jpg" style="width:100%" alt="">
+      </van-swipe-item>
     </van-swipe>
     <div class="margin-top">
       <van-grid :column-num="3">
-        <van-grid-item v-for="(item, index) in gridList" :key="index" :icon="item.icon" :text="item.text" />
+        <van-grid-item v-for="(item, index) in gridList" :key="index" :use-slot="true">
+          <van-icon :name="item.icon" size="30" :color="item.color"/>
+          <span class="margin-top">{{item.text}}</span>
+        </van-grid-item>
       </van-grid>
     </div>
     
@@ -30,27 +35,33 @@ export default {
       gridList:[
         {
           icon: "comment",
-          text: "会议培训"
+          text: "会议培训",
+          color: "#00FF99"
         },
         {
           icon: "label",
-          text: "驻场登记"
+          text: "驻场登记",
+          color: "#33FF99"
         },
         {
           icon: "todo-list",
-          text: "巡查记录"
+          text: "巡查记录",
+          color: "#6699FF"
         },
         {
           icon: "underway",
-          text: "巡查类型"
+          text: "巡查类型",
+          color: "#66CCFF"
         },
         {
           icon: "todo-list",
-          text: "待处理"
+          text: "待处理",
+          color: "#66FF99"
         },
         {
           icon: "comment-circle",
-          text: "培训主题"
+          text: "培训主题",
+          color: "#99FFCC"
         }
       ]
     }
@@ -61,12 +72,5 @@ export default {
 }
 </script>
 <style lang="scss">
-.my-swipe .van-swipe-item {
-  color: #fff;
-  font-size: 20px;
-  line-height: 150px;
-  text-align: center;
-  background-color: #39a9ed;
-}
 </style>
 
